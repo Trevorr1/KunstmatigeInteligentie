9bcc903752f076084605534e00c4938113763ea3
RMARomero <romero.ricardo09@outlook.com>
2014-12-13 13:10:52 +0100

    trimmed the graph code down

--- week1/Graph.cpp
+++ week1/Graph.cpp
@@ -90,60 +90,6 @@ void Graph::init()
 	{
 		printf("Path %d ", v->getId());
 	}
-
-	/*std::vector<std::string> vertex_names;
-
-	vertex_names.push_back("Harrisburg");  
-	vertex_names.push_back("Baltimore");   
-	vertex_names.push_back("Washington");  
-	vertex_names.push_back("Philadelphia");
-	vertex_names.push_back("Binghamton");  
-	vertex_names.push_back("Allentown");   
-	vertex_names.push_back("New York"); 
-	m_AdjacencyMap[0].push_back(Neighbour(1, 79));
-	m_AdjacencyMap[0].push_back(Neighbour(5, 81));
-
-	m_AdjacencyMap[1].push_back(Neighbour(0, 79));
-	m_AdjacencyMap[1].push_back(Neighbour(2, 39));
-	m_AdjacencyMap[1].push_back(Neighbour(3, 102));
-
-	m_AdjacencyMap[2].push_back(Neighbour(1, 39));
-
-	m_AdjacencyMap[3].push_back(Neighbour(1, 102));
-	m_AdjacencyMap[3].push_back(Neighbour(5, 62));
-	m_AdjacencyMap[3].push_back(Neighbour(6, 96));
-
-	m_AdjacencyMap[4].push_back(Neighbour(5, 133));
-
-	m_AdjacencyMap[5].push_back(Neighbour(0, 81));
-	m_AdjacencyMap[5].push_back(Neighbour(3, 62));
-	m_AdjacencyMap[5].push_back(Neighbour(4, 133));
-	m_AdjacencyMap[5].push_back(Neighbour(6, 91));
-
-	m_AdjacencyMap[6].push_back(Neighbour(3, 96));
-	m_AdjacencyMap[6].push_back(Neighbour(5, 91));
-
-	ComputePath(0, m_AdjacencyMap, m_MinimumDistance, m_Previous);*/
-
-	/*for (std::map<int, std::list<Neighbour>>::iterator vertex_iter = m_AdjacencyMap.begin(); vertex_iter != m_AdjacencyMap.end(); vertex_iter++)
-	{
-		int v = vertex_iter->first;
-		std::cout << "Distance to " << vertex_names[v] << ": " << m_MinimumDistance[v] << std::endl;
-		std::list<int> path = GetShortestPathTo(0, v, m_Previous);
-		std::list<int>::iterator path_iter = path.begin();
-		std::cout << "Path: ";
-		for (; path_iter != path.end(); path_iter++)
-		{
-			std::cout << vertex_names[*path_iter] << " ";
-		}
-		std::cout << std::endl;
-	}*/
-
-	/*std::cout << "Distance from 0 to 4: " << m_MinimumDistance[4] << std::endl;
-	std::list<int> path = GetShortestPathTo(0, 6, m_Previous);
-	std::cout << "Path : ";
-	std::copy(path.begin(), path.end(), std::ostream_iterator<int>(std::cout, " "));
-	std::cout << std::endl;*/
 }
 
 void Graph::Draw()
@@ -185,78 +131,6 @@ void Graph::Draw()
 
 	m_Cow->update();
 	m_Rabbit->update();
-}
-
-void Graph::ComputePath(int source, std::map<int, std::list<Neighbour>>& adjacencyMap, std::map<int, int>& minDistance, std::map<int, int>& previous)
-{
-	for (std::map<int, std::list<Neighbour>>::iterator vertex_iter = adjacencyMap.begin(); vertex_iter != adjacencyMap.end(); vertex_iter++)
-	{
-		int v = vertex_iter->first;
-		minDistance[v] = std::numeric_limits<int>::infinity();
-	}
-
-	minDistance[source] = 0;
-
-	std::set<std::pair<int, int>, pair_first_less<int, int>> vertex_queue;
-	for (std::map<int, std::list<Neighbour>>::iterator vertex_iter = adjacencyMap.begin(); vertex_iter != adjacencyMap.end(); vertex_iter++)
-	{
-		int v = vertex_iter->first;
-		vertex_queue.insert(std::pair<int, int>(minDistance[v], v));
-	}
-
-	//previous[source] = source;
-
-	while (!vertex_queue.empty())
-	{
-		int u = vertex_queue.begin()->second;
-		vertex_queue.erase(vertex_queue.begin());
-
-		// Visit each edge exiting u
-		for (std::list<Neighbour>::iterator edge_iter = adjacencyMap[u].begin(); edge_iter != adjacencyMap[u].end(); edge_iter++)
-		{
-			int v = edge_iter->m_Target;
-			int  weight = edge_iter->m_Weight;
-
-			//relax the edge(u, v)
-			int distance_through_u = minDistance[u] + weight;
-
-			if (minDistance[v] == 0)
-			{
-				minDistance[v] = distance_through_u;
-				previous[v] = u;
-				vertex_queue.insert(std::pair<int, int>(distance_through_u, v));
-			}
-			
-			if (distance_through_u < minDistance[v]) 
-			{
-				//remove v from queue
-				vertex_queue.erase(std::pair<int, int>(minDistance[v], v));
-
-				minDistance[v] = distance_through_u;
-				previous[v] = u;
-
-				//re - add v to queue
-				vertex_queue.insert(std::pair<int, int>(minDistance[v], v));
-			}
-		}
-	}
-}
-
-std::list<int> Graph::GetShortestPathTo(int source, int target, std::map<int, int>& previous)
-{
-	std::list<int> path;
-	std::map<int, int>::iterator prev;
-	int vertex = target;
-	path.push_front(vertex);
-
-	while ((prev = previous.find(vertex)) != previous.end())
-	{
-		vertex = prev->second;
-		path.push_front(vertex);
-		if (vertex == source)
-			break;
-	}
-	return path;
 }
 
 list<Vertex*> Graph::ComputeAStarPath(Vertex* source, Vertex* target)
--- week1/Graph.h
+++ week1/Graph.h
@@ -7,24 +7,6 @@
 
 namespace Tmpl8
 {
-	template <typename T1, typename T2>
-	struct pair_first_less
-	{
-		bool operator()(std::pair<T1, T2> p1, std::pair<T1, T2> p2)
-		{
-			return p1.first < p2.first;
-		}
-	};
-
-	class Neighbour
-	{
-	public:
-		Neighbour(int target, int weight) : m_Target(target), m_Weight(weight) {}
-		int m_Target;
-		int m_Weight;
-	private:
-	};
-
 	class Surface;
 	class Vertex;
 	class Edge;
@@ -38,10 +20,6 @@ namespace Tmpl8
 
 		void init();
 		void Draw();
-
-		//dijkstra stuff
-		void ComputePath(int source, std::map<int, std::list<Neighbour>>& adjacencyMap, std::map<int, int>& minDistance, std::map<int, int>& previous);
-		std::list<int> GetShortestPathTo(int source, int target, std::map<int, int>& previous);
 
 		//A* stuff
 		std::list<Vertex*> ComputeAStarPath(Vertex* source, Vertex* target);
@@ -61,11 +39,6 @@ namespace Tmpl8
 
 		Cow* m_Cow;
 		Rabbit* m_Rabbit;
-
-		//dijkstra stuff
-		std::map<int, std::list<Neighbour>> m_AdjacencyMap;
-		std::map<int, int> m_MinimumDistance;
-		std::map<int, int> m_Previous;
 
 		//A* stuff
 		std::vector<Vertex*>* m_OpenList;
